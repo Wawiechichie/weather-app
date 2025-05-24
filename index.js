91fd4d258d35f0433b83dbec79c830bfd2959407
Wawiechichie <wadzanaichiromo96@gmail.com>
2025-05-25 00:28:40 +0200

    updated js

--- index.js
+++ index.js
@@ -35,6 +35,9 @@ function refreshWeather(response) {
     if (minutes < 10) {
       minutes = `0${minutes}`;
     }
+    if (hours < 10) {
+      hours = `0${hours}`
+    }
   
     return `${day} ${hours}:${minutes}`;
   }
