a99e2e36bb0597a20d32d2cc6f7995531e893b46
Wawiechichie <wadzanaichiromo96@gmail.com>
2025-05-29 00:09:18 +0200

    injected more forecast funcions in js

--- index.js
+++ index.js
@@ -56,30 +56,40 @@ function refreshWeather(response) {
     searchCity(searchInput.value);
   }
 
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun","Mon","Tue","Wed","Thur","Fri","Sat"];
+
+  return days[date.getDay()];
+}
+
   function getForecast(city){
     let apiKey = "8fe22908f12at3143aof40dfb7505473"
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`
     axios(apiUrl).then(displayForecast);
   }
 
-  function displayForecast(){
-    let days = ["Wed","Thur","Fri","Sat","Sun"];
-
+  function displayForecast(response){
+    console.log(response.data);
     let forecastHTML = "";
 
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+      if (index <5) {
       forecastHTML = 
       
       forecastHTML +
       `<div class="weather-forecast-day">
-      <div class="weather-forecast-date">${day} </div>
-      <div class="weather-forecast-icon"> ⛅ </div>
+      <div class="weather-forecast-date">${formatDay(day.time)} </div>
+      <div class="weather-forecast-icon"> 
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+      </div>
       <div class="weather-forecast-temperatures"> 
-        <div class="weather-forecast-temperature"><strong>15°</strong></div> 
-        <div class="weather-forecast-temperature">9°</div>
+        <div class="weather-forecast-temperature"><strong>${Math.round(day.temperature.maximum)}°</strong></div> 
+        <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
       </div>
     </div>
     `;
+  }
     });
    
     let forecastElement = document.querySelector("#forecast");
@@ -92,7 +102,7 @@ function refreshWeather(response) {
   searchFormElement.addEventListener("submit", handleSearchSubmit);
   
   searchCity("Paris");
-  displayForecast();
+  /*displayForecast();*/
 
 
   
