2961509ccf3771a1f25e4a757caf1634a94b4725
Wawiechichie <wadzanaichiromo96@gmail.com>
2025-05-28 22:49:14 +0200

    injected forecast in js

--- index.js
+++ index.js
@@ -54,8 +54,38 @@ function refreshWeather(response) {
   
     searchCity(searchInput.value);
   }
+
+  function displayForecast(){
+    let days = ["Wed","Thur","Fri","Sat","Sun"];
+
+    let forecastHTML = "";
+
+    days.forEach(function (day) {
+      forecastHTML = 
+      
+      forecastHTML +
+      `<div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day} </div>
+      <div class="weather-forecast-icon"> ⛅ </div>
+      <div class="weather-forecast-temperatures"> 
+        <div class="weather-forecast-temperature"><strong>15°</strong></div> 
+        <div class="weather-forecast-temperature">9°</div>
+      </div>
+    </div>
+    `;
+    });
+   
+    let forecastElement = document.querySelector("#forecast");
+      forecastElement.innerHTML = forecastHTML;
+
+  }
+  
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
   
   searchCity("Paris");
+  displayForecast();
+
+
+  
